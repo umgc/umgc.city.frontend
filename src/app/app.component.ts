4472f20324a2ff80732b09dd32a3bc945aa8023b
Krystina Poling <krystina627@gmail.com>
2020-03-15 21:25:45 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UMGC-City-webclient';
+  title = 'UMGC City Admin Portal';
 }
